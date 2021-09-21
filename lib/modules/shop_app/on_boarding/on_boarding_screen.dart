66aa393cd0b4d98644f8506a96655f31fe9add8a
ahmadLubbad <ahmedlubbad2001@gmail.com>
2021-09-21 18:20:53 +0300

    on boarding screen

--- lib/modules/shop_app/on_boarding/on_boarding_screen.dart
+++ lib/modules/shop_app/on_boarding/on_boarding_screen.dart
@@ -6,6 +6,41 @@ class OnBoardingScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
+      body:buildBoardingItem(),
     );
   }
+
+
+  Widget buildBoardingItem()=> Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children:
+    [
+      Expanded(
+        child: Image(
+          image: AssetImage('assets/images/onboard_2'),
+        ),
+      ),
+      SizedBox(
+        height: 30.0,
+      ),
+      Text(
+        'Screen Title',
+        style: TextStyle(
+          fontSize: 24.0,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      SizedBox(
+        height: 15.0,
+      ),
+      Text(
+        'Screen Body',
+        style: TextStyle(
+          fontSize: 14.0,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      // PageView.builder(itemBuilder: (context,index)=>)
+    ],
+  );
 }
