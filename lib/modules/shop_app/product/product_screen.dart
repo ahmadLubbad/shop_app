e085e331bf42e0a5e981f50c7acccb7ef8d26bdc
ahmadLubbad <ahmedlubbad2001@gmail.com>
2021-11-26 21:52:06 +0200

    Grid View [Products] part 1

--- lib/modules/shop_app/product/product_screen.dart
+++ lib/modules/shop_app/product/product_screen.dart
@@ -5,44 +5,94 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shop_app/model/home_model.dart';
 import 'package:shop_app/modules/shop_app/cubit/shop_cubit.dart';
 import 'package:shop_app/modules/shop_app/cubit/state.dart';
+import 'package:shop_app/styles/colors.dart';
 
 class ProductScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return BlocConsumer<ShopCubit,ShopStates>(
-      listener: (context,state){},
-      builder: (context,state)
-      {
+    return BlocConsumer<ShopCubit, ShopStates>(
+      listener: (context, state) {},
+      builder: (context, state) {
         return ConditionalBuilder(
           condition: ShopCubit.get(context).homeModel != null,
-          builder: (context)=>productBuilder(ShopCubit.get(context).homeModel),
-          fallback:(context)=> Center(child: CircularProgressIndicator()),
+          builder: (context) =>
+              productBuilder(ShopCubit.get(context).homeModel),
+          fallback: (context) => Center(child: CircularProgressIndicator()),
         );
       },
     );
   }
 
-
-  Widget productBuilder(HomeModel model) => Column(
-    children:
-    [
-      CarouselSlider(
-        items: model.data.banners.map((e) => Image(
-          image: NetworkImage('${e.image}'),
-          width: double.infinity,
-          fit: BoxFit.cover,
-        ),).toList(),
-        options: CarouselOptions(
-          height: 250.0,
-          initialPage: 0,
-          viewportFraction: 1.0,
-          enableInfiniteScroll: true,
-          reverse: false,
-          autoPlay: true,
-          scrollDirection: Axis.horizontal,
-
+  Widget productBuilder(HomeModel model) => SingleChildScrollView(
+        child: Column(
+          children: [
+            CarouselSlider(
+              items: model.data.banners
+                  .map(
+                    (e) => Image(
+                      image: NetworkImage('${e.image}'),
+                      width: double.infinity,
+                      fit: BoxFit.cover,
+                    ),
+                  )
+                  .toList(),
+              options: CarouselOptions(
+                height: 250.0,
+                initialPage: 0,
+                viewportFraction: 1.0,
+                enableInfiniteScroll: true,
+                reverse: false,
+                autoPlay: true,
+                scrollDirection: Axis.horizontal,
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Container(
+              color: Colors.grey[300],
+              child: GridView.count(
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                crossAxisCount: 2,
+                mainAxisSpacing: 1.0,
+                crossAxisSpacing: 1.0,
+                childAspectRatio: 1 / 1.4,
+                children: List.generate(model.data.products.length,
+                    (index) => buildGridProduct(model.data.products[index])),
+              ),
+            ),
+          ],
         ),
-      ),
-    ],
-  );
+      );
+
+  Widget buildGridProduct(ProductModel model) => Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Image(
+            image: NetworkImage(model.image),
+            width: double.infinity,
+            height: 200.0,
+            fit: BoxFit.cover,
+          ),
+          Text(
+            model.name,
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+            style: TextStyle(
+              fontSize: 16.0,
+              height: 1.4,
+            ),
+          ),
+          Text(
+            '${model.price.round()}',
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+            style: TextStyle(
+              fontSize: 16.0,
+              color: defaultColor,
+            ),
+          ),
+        ],
+      );
 }
